ad75e7fe9405ee18be1de12f11638726d4caf53c
Vyacheslav <lvv_1@list.ru>
2022-02-05 23:57:42 +0300

    DZ_7

--- Full/server.py
+++ Full/server.py
@@ -1,14 +1,20 @@
 """Программа-сервер"""
 
-import socket, sys, json, argparse, select, time
-from common.variables import ACCOUNT_LOGIN, USER,CURRENT_TIME, ACTION,DEF_PORT, DEF_IP_ADDRESS, \
-    MAX_CONNECTIONS, CODE_RESPONSE, CODE_ERROR, CODE_PRESENCE, MESSAGE, MESSAGE_TEXT, SENDER
-from common.utils import get_message, send_message
+import socket
+import sys
+import argparse
 import logging
-import log.config_server_log
-from decoration import log
+import select
+import time
+import logs.config_server_log
+from common.variables import DEFAULT_PORT, MAX_CONNECTIONS, ACTION, TIME, USER, \
+    ACCOUNT_NAME, SENDER, PRESENCE, RESPONSE, ERROR, MESSAGE, MESSAGE_TEXT
+from common.utils import get_message, send_message
+from decos import log
 
-SERVER_LOGGER = logging.getLogger('server')
+# Инициализация логирования сервера.
+LOGGER = logging.getLogger('server')
+
 
 @log
 def process_client_message(message, messages_list, client):
@@ -20,22 +26,22 @@ def process_client_message(message, messages_list, client):
     :param client:
     :return:
     """
-    SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {message}')
+    LOGGER.debug(f'Разбор сообщения от клиента : {message}')
     # Если это сообщение о присутствии, принимаем и отвечаем, если успех
-    if ACTION in message and message[ACTION] == CODE_PRESENCE and CURRENT_TIME in message \
-            and USER in message and message[USER][ACCOUNT_LOGIN] == 'Guest':
-        send_message(client, {CODE_RESPONSE: 200})
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        send_message(client, {RESPONSE: 200})
         return
     # Если это сообщение, то добавляем его в очередь сообщений. Ответ не требуется.
     elif ACTION in message and message[ACTION] == MESSAGE and \
-            CURRENT_TIME in message and MESSAGE_TEXT in message:
-        messages_list.append((message[ACCOUNT_LOGIN], message[MESSAGE_TEXT]))
+            TIME in message and MESSAGE_TEXT in message:
+        messages_list.append((message[ACCOUNT_NAME], message[MESSAGE_TEXT]))
         return
     # Иначе отдаём Bad request
     else:
         send_message(client, {
-            CODE_RESPONSE: 400,
-            CODE_ERROR: 'Bad Request'
+            RESPONSE: 400,
+            ERROR: 'Bad Request'
         })
         return
 
@@ -44,7 +50,7 @@ def process_client_message(message, messages_list, client):
 def arg_parser():
     """Парсер аргументов коммандной строки"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', default=DEF_PORT, type=int, nargs='?')
+    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
     parser.add_argument('-a', default='', nargs='?')
     namespace = parser.parse_args(sys.argv[1:])
     listen_address = namespace.a
@@ -52,18 +58,19 @@ def arg_parser():
 
     # проверка получения корретного номера порта для работы сервера.
     if not 1023 < listen_port < 65536:
-        SERVER_LOGGER.critical(
+        LOGGER.critical(
             f'Попытка запуска сервера с указанием неподходящего порта '
             f'{listen_port}. Допустимы адреса с 1024 до 65535.')
         sys.exit(1)
+
     return listen_address, listen_port
 
 
-def base():
+def main():
     """Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию"""
     listen_address, listen_port = arg_parser()
 
-    SERVER_LOGGER.info(
+    LOGGER.info(
         f'Запущен сервер, порт для подключений: {listen_port}, '
         f'адрес с которого принимаются подключения: {listen_address}. '
         f'Если адрес не указан, принимаются соединения с любых адресов.')
@@ -87,7 +94,7 @@ def base():
         except OSError:
             pass
         else:
-            SERVER_LOGGER.info(f'Установлено соедение с ПК {client_address}')
+            LOGGER.info(f'Установлено соедение с ПК {client_address}')
             clients.append(client)
 
         recv_data_lst = []
@@ -108,7 +115,7 @@ def base():
                     process_client_message(get_message(client_with_message),
                                            messages, client_with_message)
                 except:
-                    SERVER_LOGGER.info(f'Клиент {client_with_message.getpeername()} '
+                    LOGGER.info(f'Клиент {client_with_message.getpeername()} '
                                 f'отключился от сервера.')
                     clients.remove(client_with_message)
 
@@ -117,7 +124,7 @@ def base():
             message = {
                 ACTION: MESSAGE,
                 SENDER: messages[0][0],
-                CURRENT_TIME: time.time(),
+                TIME: time.time(),
                 MESSAGE_TEXT: messages[0][1]
             }
             del messages[0]
@@ -125,9 +132,9 @@ def base():
                 try:
                     send_message(waiting_client, message)
                 except:
-                    SERVER_LOGGER.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
+                    LOGGER.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
                     clients.remove(waiting_client)
 
 
 if __name__ == '__main__':
-    base()
+    main()
--- Lesson_3/common/utils.py
+++ Lesson_3/common/utils.py
@@ -2,31 +2,24 @@
 
 import json
 from common.variables import MAX_PACKAGE_LENGTH, FORMAT
+from errors import IncorrectDataRecivedError, NonDictInputError
 from decoration import log
 
 
+@log
 def get_message(client):
-    '''
-    Утилита приёма и декодирования сообщения
-    принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
-    :param client:
-    :return:
-    '''
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
     if isinstance(encoded_response, bytes):
-        response = json.loads(encoded_response.decode(FORMAT))
+        json_response = encoded_response.decode(FORMAT)
+        response = json.loads(json_response)
         if isinstance(response, dict):
             return response
-        raise ValueError
-    raise ValueError
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
 
 
+@log
 def send_message(sock, message):
-    '''
-    Утилита кодирования и отправки сообщения
-    принимает словарь и отправляет его
-    :param sock:
-    :param message:
-    :return:
-    '''
+    if not isinstance(message, dict):
+        raise NonDictInputError
     sock.send(json.dumps(message).encode(FORMAT))
